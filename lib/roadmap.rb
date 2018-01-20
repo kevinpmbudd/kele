c63740f41cfc9e450da5830d58461223293cab9c
Kevin Budd <kevin@kevinbudd.com>
2018-01-20 14:46:22 -0600

    user story 4: check for auth token prior to GET requests

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -1,13 +1,21 @@
 module Roadmap
   def get_roadmap(roadmap_id)
-    url = "/roadmaps/#{roadmap_id}"
-    response = self.class.get(url, headers: { "authorization" => @auth_token })
-    JSON.parse(response.body)
+    if @auth_token
+      url = "/roadmaps/#{roadmap_id}"
+      response = self.class.get(url, headers: { "authorization" => @auth_token })
+      JSON.parse(response.body)
+    else
+      puts "Not logged in. Please log in."
+    end
   end
 
   def get_checkpoint(checkpoint_id)
-    url = "/checkpoints/#{checkpoint_id}"
-    response = self.class.get(url, headers: { "authorization" => @auth_token })
-    JSON.parse(response.body)
+    if @auth_token
+      url = "/checkpoints/#{checkpoint_id}"
+      response = self.class.get(url, headers: { "authorization" => @auth_token })
+      JSON.parse(response.body)
+    else
+      puts "Not logged in. Please log in."
+    end 
   end
 end
